15611f56a9cdb787dcc6cc6b62ea1a7394a13323
noobgod-115 <bavariakaran@gmail.com>
2020-10-21 02:12:32 +0530

    some more work done

--- src/app/components/work/work.component.ts
+++ src/app/components/work/work.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {
+  faAngular,
+  faNode,
+  faGithub,
+  faGit,
+  faGitlab,
+  faCss3,
+  faHtml5,
+  faJs,
+  faSass,
+  faNpm,
+} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-work',
   templateUrl: './work.component.html',
-  styleUrls: ['./work.component.scss']
+  styleUrls: ['./work.component.scss'],
 })
-export class WorkComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class WorkComponent {
+  iconList = [
+    faAngular,
+    faNode,
+    faGithub,
+    faGit,
+    faGitlab,
+    faCss3,
+    faHtml5,
+    faJs,
+    faSass,
+    faNpm,
+  ];
 }
